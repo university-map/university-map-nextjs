c289d6d0461b0f427b2b90b189a47e0b6aafa909
Jack Lin <jacklin@cybersoft4u.com>
2024-02-12 21:32:36 +0800

    Fix change translation bug

--- university-map/src/components/Nav/LanguagePicker.tsx
+++ university-map/src/components/Nav/LanguagePicker.tsx
@@ -3,20 +3,28 @@ import { useLocale } from 'next-intl';
 import { Select } from '@mantine/core';
 import { usePathname, useRouter } from '@/navigation';
 
-
 const languages: { [locale: string]: string; } = {
   'en': 'English',
   'zh-TW': '正體中文'
 };
 
 const LanguagePicker: React.FC = () => {
-  const router = useRouter();
-  const pathname = usePathname();
-  const locale = useLocale();
   const params = useParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  const locale = useLocale();
   const updateLanguage = async (language: string): Promise<void> => {
     const newLocale = Object.keys(languages).find((key) => languages[key] === language);
-    router.replace({pathname, params: params as any}, {locale: newLocale});
+    if (pathname !== '/university/[country]/[university]') {
+      // For routes with parametes:
+      // router.replace({pathname, params: params as any}, {locale: newLocale});
+      router.replace(pathname, {locale: newLocale});
+      return;
+    }
+
+    // '', 'en', 'university', '[country]', '[university]'
+    const segments = decodeURI(window.location.pathname).split('/');
+    router.replace({pathname, params: {country: segments[3], university: segments[4] }}, {locale: newLocale});
   };
 
   return (
